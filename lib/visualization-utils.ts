7a3930f2d4f7ef11c52e206a7f9960f408507e23
anpn <anthachby2@gmail.com>
2024-11-08 14:50:22 +0700

    refactor: move converResponseToVisualizationData func to lib folder

---
+++ lib/visualization-utils.ts
@@ -0,0 +1,61 @@
+import { Node, Edge } from "./elk";
+
+export type IResponse = {
+  result: {
+    name: string;
+    value: string[];
+    detail?: string;
+  }[];
+  tables: {
+    name: string;
+    columns: string[];
+  }[];
+};
+
+export function convertResponseToVisualizationData(response: IResponse): {
+  nodes: Node[];
+  edges: Edge[];
+} {
+  const nodes: Node[] = [];
+  const edges: Edge[] = [];
+
+  // Create nodes for each table
+  response.tables.forEach((table) => {
+    nodes.push({
+      id: table.name,
+      data: { label: table.name },
+      type: "table",
+      children: table.columns.map((column) => ({
+        id: `${table.name}.${column}`,
+        type: "column",
+        data: { label: column },
+      })),
+    });
+  });
+
+  // add table result as nodes
+  nodes.push({
+    id: "result",
+    data: { label: "result" },
+    type: "result",
+    children: response.result.map((item) => ({
+      id: `result.${item.name}`,
+      data: { label: item.name },
+      detail: item.detail,
+      type: "column",
+    })),
+  });
+
+  // Create nodes and edges for computed columns/relationships
+  response.result.forEach((item) => {
+    item.value.forEach((column) => {
+      edges.push({
+        id: `${column}-result.${item.name}`,
+        source: column,
+        target: `result.${item.name}`,
+      });
+    });
+  });
+
+  return { nodes, edges };
+}
